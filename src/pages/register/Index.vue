bccd75760e3775a159b5001d7a353132c537b062
MeasponeS <958183117qq.com>
2019-01-03 20:25:07 +0800

    注册页面完成
    注册页面

--- src/pages/register/Index.vue
+++ src/pages/register/Index.vue
@@ -1,17 +1,18 @@
 <template>
     <div id="app">
         <Head></Head>
-        <div class="container main-body">
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
-
-
-
+        <div class="back container-fluid main-body ">
+            <div class="content">
+                <h1>注册</h1>
+                <h2></h2>
+                <input type="text" placeholder="手机号">
+                <input type="text" placeholder="验证码" class="input1">
+                <a href="##">获取验证码</a>
+                <input type="password" placeholder="设置密码不少于8位" class="input2">
+                <button class="button">保存密码</button>
+            </div>
         </div>
+        
         <Footer></Footer>
     </div>
 </template>
