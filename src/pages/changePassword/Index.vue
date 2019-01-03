baf2100c8a1ae8c7ed83073eb35580710ab34025
MeasponeS <958183117qq.com>
2019-01-03 18:42:32 +0800

    修改密码页
    修改密码

---
+++ src/pages/changePassword/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <Head></Head>
+        <div class="back">
+            <div class="content">
+                <h1>修改密码</h1>
+                <h2></h2>
+                <input type="text" placeholder="手机号">
+                <input type="text" placeholder="验证码">
+                <a href="##">获取验证码</a>
+                <input type="password" placeholder="设置密码">
+                <button class="button">保存密码</button>
+            </div>
+        </div>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'app',
+data: function () {
+return {
+}
+},
+methods: {},
+mounted() {
+
+},
+beforeDestroy: function () {
+
+},
+components: {}
+}
+</script>
