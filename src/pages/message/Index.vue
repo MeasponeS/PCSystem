353bc190a691de57b1a8e12ef5ccf94fe44fb277
MeasponeS <958183117qq.com>
2019-01-05 19:03:01 +0800

    消息页

--- src/pages/message/Index.vue
+++ src/pages/message/Index.vue
@@ -1,16 +1,54 @@
 <template>
     <div id="app">
         <Head></Head>
-        <div class="container main-body">
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
-
-
-
+        <div class="wrapper">
+            <div class="container main-body">
+                <div class="leftTop">
+                    消息列表 > 
+                    <span>消息详情</span>
+                </div>
+                <div class="rightTop">消息列表</div>
+                <div class="leftBottom">
+                    <div class="all">
+                        <img src="./img/message.png" alt="">
+                        <div class="content">
+                            <strong>这里是消息标题</strong>
+                            <span>2018.12.19 13:00:00</span>
+                            <p>这是正文正文文这是正文正文正文这是正文正文正文这是正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文正文这是正文正文正文</p>
+                            <a href="##">这里是可能出现的考场链接、课程链接</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="rightBottom">
+                    <ul>
+                        <li>
+                            <span class="unread">未读</span>
+                            <strong>2018年执业护士考试测评第一次</strong>
+                            <h4>2018.12.19 13:00:05</h4>
+                        </li>
+                        <li>
+                            <span class="read">已读</span>
+                            <strong>有可能长有可能短是不是很对</strong>
+                            <h4>2018.12.19 13:00:04</h4>
+                        </li>
+                        <li>
+                            <span class="read">已读</span>
+                            <strong>不知道是不是有可能没有标题</strong>
+                            <h4>2018.12.19 13:00:03</h4>
+                        </li>
+                        <li>
+                            <span class="read">已读</span>
+                            <strong>这里是消息标题</strong>
+                            <h4>2018.12.19 13:00:02</h4>
+                        </li>
+                        <li>
+                            <span class="read">已读</span>
+                            <strong>不知道放这里合不合适啊</strong>
+                            <h4>2018.12.19 13:00:01</h4>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
         <Footer></Footer>
     </div>
