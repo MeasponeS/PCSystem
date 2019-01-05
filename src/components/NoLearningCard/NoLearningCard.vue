22cc0e574667640a4033710ea5654fab8145e200
737657495@qq.com <1468756621306qq>
2019-01-05 11:10:28 +0800

    添加弹框

---
+++ src/components/NoLearningCard/NoLearningCard.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="no-learning-card">
+        <el-dialog
+                title="提示"
+                :visible.sync="value"
+                custom-class="no-learning-card-dialog"
+                :show-close="false"
+                :close-on-click-modal="false"
+                >
+            <span slot="title"></span>
+            <div class="dialog-content">
+                <img class="kf" src="./img/kf.png" alt="">
+                <p class="info">
+                    您还没有获得学习卡，请联系客服进行购买
+                    <br>
+                    联系电话：{{ phone }}
+                </p>
+
+                <el-button @click="$emit('finish')" type="primary" style="width: 100%">完成</el-button>
+                <img @click="$emit('input',false)" class="close" src="../../assets/img/close.png" alt="">
+            </div>
+
+        </el-dialog>
+
+    </div>
+</template>
+
+<script>
+    import Config from '../../config/app.js'
+    export default {
+        name: 'NoLearningCard',
+        props: {
+            value:Boolean,
+            phone:[String , Number]
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+    @import "../../assets/css/var.scss";
+    .no-learning-card{
+        .el-dialog__wrapper{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .el-dialog__header{
+                padding: 0;
+                height: 0;
+            }
+            .no-learning-card-dialog{
+                margin-top: 0!important;
+                margin: 0;
+                width: 424px;
+                height: 404px;
+                .el-dialog__body{
+                    padding: 48px 20px;
+                }
+                .dialog-content{
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    .kf{
+                        width: 178px;
+                        height: 138px;
+                    }
+                    .info{
+                        line-height: 36px;
+                        text-align: center;
+                        color: #333;
+                        font-size: 20px;
+                        margin: 34px 0;
+                    }
+                    .close{
+                        cursor: pointer;
+                        width: 40px;
+                        height: 40px;
+                        position: absolute;
+                        bottom: -56px;
+                    }
+                }
+
+            }
+        }
+
+    }
+</style>
--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -1,33 +1,37 @@
 <template>
     <div id="app">
         <Head activeUrl="study"></Head>
-        <img style="width: 100%" src="./img/banner.jpg" alt="">
-        <div class="container main-body">
-
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
-            <el-date-picker
-                    v-model="value1"
-                    type="datetime"
-                    placeholder="选择日期时间">
-            </el-date-picker>
+        <div class="container-fluid main-body">
+            <img style="width: 100%" src="./img/banner.jpg" alt="">
+            <div class="container">
+                <el-button>默认按钮</el-button>
+                <el-button type="primary">主要按钮</el-button>
+                <el-button>默认按钮</el-button>
+                <el-button type="primary">主要按钮</el-button>
+                <el-button type="success">成功按钮</el-button>
+                <el-button type="info">信息按钮</el-button>
+                <el-button type="warning">警告按钮</el-button>
+                <el-button type="danger">危险按钮</el-button>
+                <el-date-picker
+                        v-model="value1"
+                        type="datetime"
+                        placeholder="选择日期时间">
+                </el-date-picker>
+            </div>
         </div>
         <Footer></Footer>
+        <NoLearningCard v-model="NoLearningCard" :phone="12345678901"></NoLearningCard>
     </div>
 </template>
 
 <script>
+    import NoLearningCard from '../../components/NoLearningCard/NoLearningCard.vue'
     export default {
         name: 'app',
         data: function () {
             return {
-                value1:''
+                value1:'',
+                NoLearningCard:true
             }
         },
         methods: {},
@@ -37,7 +41,7 @@
         beforeDestroy: function () {
 
         },
-        components: {}
+        components: {NoLearningCard}
     }
 </script>
 
