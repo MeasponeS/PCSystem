30d532a09db03f5fe8f21f0bd74db10862e613e4
MeasponeS <958183117qq.com>
2019-01-05 16:35:45 +0800

    考试列表页
    考试列表页

--- src/pages/exam/Index.vue
+++ src/pages/exam/Index.vue
@@ -12,107 +12,107 @@
             <div v-show="status=='opening'">
                 <div class="courseListOne">
                     <h1>健康管理师课程类</h1>
-                    <span>当前您正在学习健康管理师系列课程，已完成10%，比其他学员的进度快1.1%</span>
+                    <span class="flag">当前您正在学习健康管理师系列课程，已完成10%，比其他学员的进度快1.1%</span>
                     <ul class="examList">
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>进行中</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button>进入考场</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">进入考场</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>进行中</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button>进入考场</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">进入考场</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>进行中</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button>进入考场</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">进入考场</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>进行中</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button>进入考场</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">进入考场</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>进行中</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button>进入考场</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">进入考场</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>进行中</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button>进入考场</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">进入考场</el-button></h5>
+                            
                         </li>
                     </ul>
                 </div>
                 <div class="courseListTwo">
                     <h1>主管护师课程类</h1>
-                    <span>当前您正在学习主管护师系列课程，已完成10%，比其他学员的进度快1.1%</span>
+                    <span class="flag">当前您正在学习主管护师系列课程，已完成10%，比其他学员的进度快1.1%</span>
                     <ul class="examList">
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>已结束</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道(答对18道)</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button class="detail">查看详情</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">查看详情</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>已结束</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button class="no">您没有参与这场考试</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary" class="not">您没有参与这场考试</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>已结束</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道(答对18道)</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button class="detail">查看详情</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary">查看详情</el-button></h5>
+                            
                         </li>
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>已结束</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button class="no">您没有参与这场考试</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary" class="not">您没有参与这场考试</el-button></h5>
+                            
                         </li>
                     </ul>
                 </div>
                 <div class="courseListThree">
                     <h1>主管护师课程类</h1>
-                    <span>当前您正在学习主管护师系列课程，已完成10%，比其他学员的进度快1.1%</span>
+                    <span class="flag">当前您正在学习主管护师系列课程，已完成10%，比其他学员的进度快1.1%</span>
                     <ul class="examList">
                         <li>
                             <h1>1220 模拟考试</h1>
                             <i>已结束</i>
                             <h4>开始时间：2018.12.29-2019.01.03</h4>
                             <h4>试题数量：20道</h4>
-                            <h5><em>考试时长：10分钟</em></h5>
-                            <button class="no">您没有参与这场考试</button>
+                            <h5><em>考试时长：10分钟</em><el-button type="primary" class="not">您没有参与这场考试</el-button></h5>
+                            
                         </li>
                     </ul>
                 </div>
