70aad9c6845a508a385fd3c6b36c20fdfbdd1e23
sunbangbang <sunbangbang@hulian120.com>
2019-01-06 10:32:23 +0800

    更改传值

--- src/components/Breadcrumb/Breadcrumb.vue
+++ src/components/Breadcrumb/Breadcrumb.vue
@@ -2,11 +2,11 @@
   <el-breadcrumb separator-class="el-icon-arrow-right">
     <el-breadcrumb-item><slot name="header"></slot></el-breadcrumb-item>
     <el-breadcrumb-item><slot></slot></el-breadcrumb-item>
-    <el-breadcrumb-item :to="{path:pathUrl}"><slot name="footer"></slot></el-breadcrumb-item>
+    <el-breadcrumb-item><slot name="footer"></slot></el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 <script>
 export default {
-  props: ['pathUrl']
+  
 }
 </script>
