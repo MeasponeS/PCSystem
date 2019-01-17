8d2197d27089ad04d221b38efa87d41b3761d51d
737657495@qq.com <1468756621306qq>
2019-01-17 09:46:10 +0800

    考试

---
+++ src/api/exam.js
@@ -0,0 +1,18 @@
+import request from "../utils/request";
+
+export function getRoomListByType(params) {
+    return request({
+        url: '/pc/evaluation/getRoomListByType',
+        method: 'get',
+        params: params
+    })
+}
+
+
+export function getExam(params) {
+    return request({
+        url: '/pc/evaluation/getExam',
+        method: 'get',
+        params: params
+    })
+}
